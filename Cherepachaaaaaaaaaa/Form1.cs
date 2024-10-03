846d08a2353fc1b99539e0da0b2172fc491a5372
user <user@BLUEPC5>
2024-10-03 20:46:42 +0300

    Добавьте файлы проекта.

---
+++ Cherepachaaaaaaaaaa/Form1.cs
@@ -0,0 +1,52 @@
+namespace Cherepachaaaaaaaaaa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Commands> commands = new List<Commands>();
+            string text = textBox1.Text + "\n";
+            while (text != "")
+            {
+                string line = text[0..text.IndexOf("\n")];
+                try
+                {
+                    commands.Add(
+                    new(
+                        line[0..line.IndexOf("(")],
+                        line[(line.IndexOf("(") + 1)..line.IndexOf(")")]
+                    )
+                    );
+                }
+                catch (Exception ex) { }
+                text = text[(text.IndexOf("\n") + 1)..^0];
+                int x;
+                if (int.TryParse(commands[commands.Count - 1].value, out x) == false)
+                {
+                    for (int i = 0; i < commands.Count; i++)
+                    {
+                        if (commands[commands.Count - 1].value == commands[i].key)
+                            commands[commands.Count - 1].value = commands[i].value;
+                    }
+                }
+                if (commands[commands.Count - 1].key == "for")
+                {
+                    string forline = text[0..text.IndexOf("}")];
+                    for (int i = 0; i < int.Parse(commands[commands.Count - 1].value) - 1; i++)
+                        text = text.Insert(text.IndexOf("}"), forline);
+                    text = text.Replace("}", "");
+                }
+            }
+            Form2 form2 = new Form2(commands); form2.Show();
+        }
+        public class Commands(string key, string value)
+        {
+            public string key = key;
+            public string value = value;
+        }
+    }
+    }
---
+++ Cherepachaaaaaaaaaa/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Cherepachaaaaaaaaaa.Form1;
+namespace Cherepachaaaaaaaaaa
+{
+    public partial class Form2 : Form
+    {
+        private List<Commands> commands = new List<Commands>();
+        public Form2(List<Commands> commands)
+        {
+            InitializeComponent();
+            this.commands = commands;
+        }
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new(Color.Black);
+            int x = 200;
+            int y = 200;
+            float angel = 0;
+            for (int i = 0; i < commands.Count; i++)
+            {
+                Point point1 = new(x, y);
+                if (commands[i].key == "right")
+                    angel += float.Parse(commands[i].value);
+                if (commands[i].key == "left")
+                    angel -= float.Parse(commands[i].value);
+                if (commands[i].key == "forward")
+                {
+                    y += (int)(Math.Sin(angel / 180 * Math.PI) * float.Parse(commands[i].value));
+                    x += (int)(Math.Cos(angel / 180 * Math.PI) * float.Parse(commands[i].value));
+                }
+                Point point2 = new(x, y);
+                e.Graphics.DrawLine(pen, point1, point2);
+            }
+        }
+    }
+}
